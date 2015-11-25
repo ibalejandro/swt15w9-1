2b1c956346fb579f5612226f0ecd9a37b9cbff56
The_Villager <thevillager@arcor.de>
2015-11-25 20:41:30 +0100

    Merge Vorbereitung

--- prototypes/goods/src/main/java/goods/DemoApplication.java
+++ prototypes/goods/src/main/java/goods/DemoApplication.java
@@ -1,6 +1,7 @@
 package goods;
 
-import java.util.NoSuchElementException;
+import java.util.Vector;
+import java.util.Arrays;
 
 import javax.annotation.PostConstruct;
 
@@ -21,17 +22,29 @@ public class DemoApplication {
     
     @PostConstruct
     void startup(){
-    	languages=new String[]{"Deutsch","English"};
-    	Goods Fahrrad = new Goods();
-    	TexBlock Yellow = new TexBlock(new String[]{"Gelb","Yellow"},new TexBlock[]{});
-    	DB.save(Yellow);
-    	TexBlock Colour = new TexBlock(new String[]{"Farbe","Colour"},new TexBlock[]{Yellow});
-    	DB.save(Colour);
-    		
-    	for(TexBlock tex:DB.findAll()){
-    		System.out.println(tex.getTranslation(0));
-    		System.out.println(tex.getTranslation(1));
-    	}
+    	languages=new String[]{"Deutsch","English","Test"};
+    	//Goods Fahrrad = new Goods();
+    	//Vector<String> YellowV = new Vector<String>();
+    	//YellowV.add("Gelb");
+    	//YellowV.add("Yellow");
+    	//TexBlock Yellow = new TexBlock(YellowV);
+    	//DB.save(Yellow);
+    	//Vector<String> ColourV = new Vector<String>();
+    	//ColourV.add("Farbe");
+    	//ColourV.add("Colour");
+    	//Vector<TexBlock> ColourV2 = new Vector<TexBlock>();
+    	//ColourV2.add(Yellow);
+    	//TexBlock Colour = new TexBlock(ColourV,ColourV2);
+    	//DB.save(Colour);
+    	//DB.findOne(1L).addnewTranslation("Gelb2");
+    	//DB.findOne(2L).addnewTranslation("Yellow2");
+    	//DB.flush();
+    	//for(TexBlock tex:DB.findAll()){
+    	//	for(int i=0; i<tex.getLength();i++){
+    	//		System.out.println(tex.getTranslation(i));
+    	//	}
+    	//}
+    	
     		
     }
 }
--- prototypes/goods/src/main/java/goods/TexBlock.java
+++ prototypes/goods/src/main/java/goods/TexBlock.java
@@ -1,7 +1,8 @@
 package goods;
 
-import java.util.Optional;
+import java.util.Vector;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,49 +12,73 @@ import org.springframework.util.Assert;
 @Entity
 public class TexBlock implements java.io.Serializable {
 	
-	private @Id @GeneratedValue Long ID;
-	private TexBlock[] blockvalues;
-	private String[] blocktranslations;
+	@Id @GeneratedValue
+	private  Long ID;
 	
-	public TexBlock(String[] translations, TexBlock[] blockvalues){
-		assert blockvalues==new TexBlock[]{} : "Induktionsalarm!";
+	@Column(length=9001)
+	private Vector<TexBlock> blockvalues;
+	@Column
+	private Vector<String> blocktranslations;
+	
+	//"blocktranslations" ist der Name des Blocks in den verschiedenen Sprachen, "blockvalues" beinhaltet die als Antwortmöglichkeiten zugeordneten Textblöcke. Z. b. "Farbe" wird "Gelb","Rot", etc. zugeordnet.
+	
+	public TexBlock(Vector<String> translations, Vector<TexBlock> blockvalues){
 		Assert.notEmpty(translations, "Keine Übersetzungen angegeben!");
-		assert translations.length==DemoApplication.languages.length : "Nicht genügend Übersetzungen angegeben!";
+		assert translations.size()==DemoApplication.languages.length : "Nicht genügend Übersetzungen angegeben!";
+		this.blocktranslations= new Vector<String>();
 		this.blocktranslations=translations;
+		this.blockvalues=new Vector<TexBlock>();
 		this.blockvalues=blockvalues;
 	}
 	
-	public TexBlock(String[] translations){
+	public TexBlock(Vector<String> translations){
 		Assert.notEmpty(translations, "Keine Übersetzungen angegeben!");
-		assert translations.length==DemoApplication.languages.length : "Nicht genügend Übersetzungen angegeben!";
+		assert translations.size()==DemoApplication.languages.length : "Nicht genügend Übersetzungen angegeben!";
+		this.blocktranslations= new Vector<String>();
 		this.blocktranslations=translations;
-		this.blockvalues=new TexBlock[]{null};
+		this.blockvalues=new Vector<TexBlock>();
+		this.blockvalues=null;
 	}
 	
 	private TexBlock(){
+		this.blocktranslations= new Vector<String>();
 		this.blocktranslations=null;
+		this.blockvalues=new Vector<TexBlock>();
 		this.blockvalues=null;
 	}
 	
 	public String  getTranslation(int languageid){
-		return "Die Übersetzung von TextBlock #"+ID+" nach "+DemoApplication.languages[languageid]+" wurde angefordert. Sie lautet: "+blocktranslations[languageid];
+		return "Die Übersetzung von TextBlock #"+ID+" nach "+DemoApplication.languages[languageid]+" wurde angefordert. Sie lautet: "+this.blocktranslations.get(languageid);
 	}
 	
 	public void setTranslation(String translations, int languageid){
 		Assert.notNull(translations, "Sie müssen eine neue Übersetzung eingeben!");
 		assert languageid<DemoApplication.languages.length : "Es konnte keine Sprache für diesen Sprachindex gefunden werden.";
-		this.blocktranslations[languageid]=translations;
+		this.blocktranslations.set(languageid,translations);
+	}
+	
+	public int getLength(){
+		return this.blocktranslations.size();
 	}
 	
 	public Long getID(){
 		return ID;
 	}
 	
-	public TexBlock[] getValues(){
+	public Vector<TexBlock> getValues(){
 		return blockvalues;
 	}
 	
-	public void setValues(TexBlock[] blockvalues){
+	public void setValues(Vector<TexBlock> blockvalues){
 		this.blockvalues=blockvalues;
+	}
+	
+	public void addnewValue(TexBlock newValue){
+		this.blockvalues.add(newValue);
+	}
+	
+	public void addnewTranslation(String newTranslation){
+		this.blocktranslations.add(newTranslation);
+		System.out.println(this.blocktranslations.indexOf(newTranslation));
 	}
 }
