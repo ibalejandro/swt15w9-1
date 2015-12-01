1ab7fc8d7e60c44333de03d2588da3c3bf160a18
Mario Henze <mario.henze.94@gmail.com>
2015-12-01 18:54:22 +0100

    Added submit form for new dialog

--- app/src/main/java/app/controller/DialogController.java
+++ app/src/main/java/app/controller/DialogController.java
@@ -2,7 +2,10 @@ package app.controller;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.salespointframework.useraccount.UserAccount;
+import org.salespointframework.useraccount.UserAccountManager;
 import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -23,15 +26,19 @@ import app.repository.DialogRepository;
 @PreAuthorize("isAuthenticated()")
 public class DialogController {
 	private final UserRepository userRepository;
+	private final UserAccountManager userAccountManager;
 	private final DialogRepository dialogList;
 
 	@Autowired
-	public DialogController(DialogRepository dialogList, UserRepository userRepository) {
+	public DialogController(DialogRepository dialogList, UserRepository userRepository,
+			UserAccountManager userAccountManager) {
 		Assert.notNull(dialogList);
 		Assert.notNull(userRepository);
+		Assert.notNull(userAccountManager);
 
 		this.dialogList = dialogList;
 		this.userRepository = userRepository;
+		this.userAccountManager = userAccountManager;
 	}
 
 	@RequestMapping(value = "/dialog/{id}", method = RequestMethod.GET)
@@ -42,25 +49,59 @@ public class DialogController {
 	}
 
 	@RequestMapping(value = "/dialogList", method = RequestMethod.GET)
-	public String dialogList(Model model,@LoggedIn Optional<UserAccount> loggedInUserAccount) {
+	public String dialogList(Model model, @LoggedIn Optional<UserAccount> loggedInUserAccount) {
 		if (!loggedInUserAccount.isPresent()) {
 			return "errorpage0_empty";
 		}
 		User loggedInUser = userRepository.findByUserAccount(loggedInUserAccount.get());
-		
+
 		model.addAttribute("dialogList", loggedInUser.getDialogs());
+		for (Dialog dialog : loggedInUser.getDialogs()) {
+			System.out.println(dialog.getTitle());
+		}
 		return "dialogList";
 	}
 
+	@RequestMapping(value = "/startDialog", method = RequestMethod.GET)
+	public String newDialog() {
+		return "startDialog";
+	}
+
+	@RequestMapping(value = "/startDialog", method = RequestMethod.POST)
+	public String postNewDialog(HttpServletRequest request, @LoggedIn Optional<UserAccount> loggedInUserAccount) {
+		String title = request.getParameter("title");
+		String participant = request.getParameter("participant");
+
+		if (!loggedInUserAccount.isPresent()) {
+			return "noUser";
+		}
+
+		User loggedInUser = userRepository.findByUserAccount(loggedInUserAccount.get());
+		Optional<UserAccount> participantAccount = userAccountManager.findByUsername(participant);
+		if (!participantAccount.isPresent()) {
+			System.err.println("Couldn't find participant: " + participant);
+			return "noUser";
+		}
+		User participantUser = userRepository.findByUserAccount(participantAccount.get());
+		
+		Dialog d = new Dialog(title, loggedInUser, participantUser);
+		dialogList.save(d);
+		loggedInUser.addDialog(d);
+		participantUser.addDialog(d);
+
+		return "redirect:/dialogList";
+	}
+
 	@RequestMapping(value = "/dialogList", method = RequestMethod.POST)
-	public String saveDialog(@RequestParam("otherUser") User user, @LoggedIn Optional<UserAccount> loggedInUserAccount, @RequestParam("title") String title) {
+	public String saveDialog(@RequestParam("otherUser") User user, @LoggedIn Optional<UserAccount> loggedInUserAccount,
+			@RequestParam("title") String title) {
 		if (!loggedInUserAccount.isPresent()) {
 			return "errorpage0_empty";
 		}
 		User loggedInUser = userRepository.findByUserAccount(loggedInUserAccount.get());
-		
+
 		dialogList.save(new Dialog(title, loggedInUser, user));
-		
+
 		return "dialogList";
 	}
 }
