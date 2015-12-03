27455426b46b92f74ca7b1e6f0400c2256c7215d
Mario Henze <mario.henze.94@gmail.com>
2015-12-03 19:41:50 +0100

    Enabled .war deployment

--- app/src/main/java/app/Application.java
+++ app/src/main/java/app/Application.java
@@ -4,20 +4,26 @@ import org.salespointframework.EnableSalespoint;
 import org.salespointframework.SalespointSecurityConfiguration;
 import org.salespointframework.SalespointWebConfiguration;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
-
-
 @EnableSalespoint
-public class Application {
-	public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
+public class Application extends SpringBootServletInitializer {
 	private static final String LOGIN_ROUTE = "/login";
 
-	//Übernommen aus Videoshop, Modifikation nötig
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
+	// Übernommen aus Videoshop, Modifikation nötig
 	@Configuration
 	static class UserManagementApplicationWebConfiguration extends SalespointWebConfiguration {
 		@Override
@@ -25,13 +31,14 @@ public class Application {
 			registry.addViewController(LOGIN_ROUTE).setViewName("login");
 		}
 	}
-	
+
 	@Configuration
 	static class WebSecurityConfiguration extends SalespointSecurityConfiguration {
 
 		/**
-		 * Disabling Spring Security's CSRF support as we do not implement pre-flight request handling for the sake of
-		 * simplicity. Setting up basic security and defining login and logout options.
+		 * Disabling Spring Security's CSRF support as we do not implement
+		 * pre-flight request handling for the sake of simplicity. Setting up
+		 * basic security and defining login and logout options.
 		 * 
 		 * @see org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter#configure(org.springframework.security.config.annotation.web.builders.HttpSecurity)
 		 */
@@ -40,12 +47,8 @@ public class Application {
 
 			http.csrf().disable();
 
-			http.authorizeRequests().antMatchers("/**").permitAll().and().//
-					formLogin().loginPage(LOGIN_ROUTE).loginProcessingUrl(LOGIN_ROUTE).and(). //
-					logout().logoutUrl("/logout").logoutSuccessUrl("/");
+			http.authorizeRequests().antMatchers("/**").permitAll().and().formLogin().loginPage(LOGIN_ROUTE)
+					.loginProcessingUrl(LOGIN_ROUTE).and().logout().logoutUrl("/logout").logoutSuccessUrl("/");
 		}
 	}
 }
-
-
-
