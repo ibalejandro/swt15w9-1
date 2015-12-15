55252e08f6a4e00434b29baae46bcd75160cadc8
Mario Henze <mario.henze.94@gmail.com>
2015-12-15 18:34:26 +0100

    Textblocks can be generated through web interface

---
+++ app/src/main/java/app/controller/TextblockController.java
@@ -0,0 +1,57 @@
+package app.controller;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.util.Assert;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import app.repository.TextBlockRepository;
+import app.textblocks.TextBlock;
+
+@Controller
+@PreAuthorize("isAuthenticated()")
+public class TextblockController {
+	private final TextBlockRepository textBlockRepository;
+
+	@Autowired
+	public TextblockController(TextBlockRepository tbr) {
+		Assert.notNull(tbr);
+
+		this.textBlockRepository = tbr;
+	}
+
+	@RequestMapping(value = "/textBlockList", method = RequestMethod.GET)
+	public String showTextBlocks(Model model) {
+		Iterable<TextBlock> itb = textBlockRepository.findAll();
+		List<String> tbFormatStrings = new LinkedList<>();
+		
+		for (TextBlock textBlock : itb) {
+			tbFormatStrings.add(textBlock.getFormatString());
+		}
+		
+		model.addAttribute("textBlocks", tbFormatStrings);
+		
+		return "textBlockList";
+	}
+	
+	@RequestMapping(value = "/newTextBlock", method = RequestMethod.GET)
+	public String showNewTextBlock() {
+		return "newTextBlock";
+	}
+	
+	@RequestMapping(value = "/newTextBlock", method = RequestMethod.POST)
+	public String newTextBlock(HttpServletRequest request) {
+		//TODO: delete in final release!
+		System.out.println(request.getParameter("formatString"));
+		
+		return "newTextBlock";
+	}
+}
