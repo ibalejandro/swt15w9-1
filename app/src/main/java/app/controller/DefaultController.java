aa0f598584794f8fc0bf417aa6bb0e7ae001c668
Lennart Schmidt <LennartProjekt@gmail.com>
2015-12-15 23:23:16 +0100

    Weitere Design Änderrungen und Anpassungen.

--- app/src/main/java/app/controller/DefaultController.java
+++ app/src/main/java/app/controller/DefaultController.java
@@ -1,23 +1,16 @@
 package app.controller;
 
-import java.util.List;
-import java.util.Optional;
-
 import javax.servlet.http.HttpServletRequest;
 
-import org.salespointframework.useraccount.UserAccount;
-import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import app.model.GoodEntity;
-import app.model.User;
+
 import app.model.UserRepository;
 import app.repository.GoodsRepository;
 import app.repository.TagsRepository;
-import app.util.Tuple;
 
 @Controller
 public class DefaultController {
@@ -43,7 +36,7 @@ public class DefaultController {
 	   * This method is the answer for the request to '/index' or '/' and redirects to the main page (index template).
 	   */
 	@RequestMapping({"/","/index"})
-	String index(HttpServletRequest request, ModelMap modelMap){
+	String index(HttpServletRequest request, Model modelMap){
 //		translation(modelMap);
 		modelMap.addAttribute("result", goodsRepository.findAll());
 		return "index";
