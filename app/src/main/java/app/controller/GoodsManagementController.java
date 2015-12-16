c50cd63b2507f5b497c15dc45ecf706320307314
ibalejandro <asanch41@eafit.edu.co>
2015-12-16 18:19:26 +0100

    Validations for the processes offer, manage and search good are made to prevent errors and to add security and coherence.

--- app/src/main/java/app/controller/GoodsManagementController.java
+++ app/src/main/java/app/controller/GoodsManagementController.java
@@ -7,11 +7,15 @@ import org.salespointframework.useraccount.web.LoggedIn;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import app.repository.TagsRepository;
+import validator.GoodValidator;
 import app.model.GoodEntity;
 import app.model.TagEntity;
 import app.model.User;
@@ -33,6 +37,8 @@ public class GoodsManagementController {
 	private final UserRepository userRepository;
 	private final GoodsRepository goodsRepository;
 	private final TagsRepository tagsRepository;
+	
+	private static final String POST = "POST";
 
 	/**
    * Autowire.
@@ -76,30 +82,55 @@ public class GoodsManagementController {
    * retrieves the particular good that the user wants to update.
    * @param HttpServletRequest The request with its information
    * @param Model The model to add response's attributes
+   * @param GoodEntity The good to be refilled and updated
    * @param Optional<UserAccount> The user's account who wants to update one of
    *                              his offered goods
    * @return String The name of the view to be shown after processing
    */
-	@RequestMapping(value = "/update", method = RequestMethod.POST)
-  public String showGoodToUpdate(HttpServletRequest request, Model model, 
+	@RequestMapping(value = "/update", 
+	                method = {RequestMethod.GET, RequestMethod.POST})
+  public String showGoodToUpdate(HttpServletRequest request, Model model,
+                                 @ModelAttribute("good") GoodEntity good,
                                  @LoggedIn Optional<UserAccount> userAccount) {
-	  long id = Long.parseLong(request.getParameter("id"));
-
-	  if (!userAccount.isPresent()) return "noUser";
+	  long id;
 	  
-		GoodEntity good = goodsRepository.findOne(id);
+	  /*
+	   * This condition is made because the way parameters are read is different 
+	   * for get and post requests.
+	   */
+	  if (request.getMethod().equals(POST)) {
+	    id = Long.parseLong(request.getParameter("id"));
+	  }
+	  else id = (Long) model.asMap().get("id");
+	  
+	  if (!userAccount.isPresent()) return "noUser";
+	  User loggedUser = userRepository.findByUserAccount(userAccount.get());
+	  
+		GoodEntity goodToUpdate = goodsRepository.findOne(id);
+    
+    // If the good is null, the user is redirected to its offered goods again.
+    if (goodToUpdate == null) {
+      model.addAttribute("wantedAction", "update");
+      return "noSuchGood";
+    }
+    
+    /*
+     * If the good's owner is different from the user trying to update the good,
+     * the user is redirected to its offered goods again.
+     */
+    if (goodToUpdate.getUser() != loggedUser) {
+      model.addAttribute("wantedAction", "update");
+      return "notYourGood";
+    }
 
-		// If the entity doesn't exist, an empty entity is created.
-		if (good == null) good = GoodEntity.createEmptyGood();
-
-		model.addAttribute("result", good);
+		model.addAttribute("good", goodToUpdate);
 		/*
 		 * Every tag is sent to the update view except for the current tag. The
 		 * current tag is already known and it's put as the default value whereas
 		 * the other tags are there, so that the user can change the existing one.
 		 */
 		model.addAttribute("tags", tagsRepository
-		                   .findByIdNot(good.getTag().getId()));
+		                   .findByIdNot(goodToUpdate.getTag().getId()));
 		return "update";
   }
 
@@ -109,12 +140,17 @@ public class GoodsManagementController {
    * good.
    * @param HttpServletRequest The request with its information
    * @param Model The model to add response's attributes
+   * @param GoodEntity The good to be updated
+   * @param BindingResult The parameter in charge of the validation result
    * @param Optional<UserAccount> The user's account who wants to update one of
    *                              his offered goods
    * @return String The name of the view to be shown after processing
    */
 	@RequestMapping(value = "/updatedGood", method = RequestMethod.POST)
-  public String updateGood(HttpServletRequest request, Model model,
+  public String updateGood(HttpServletRequest request, Model model, 
+                           @ModelAttribute("good") GoodEntity good, 
+                           BindingResult bindingResult,
+                           RedirectAttributes redirectAttributes,
                            @LoggedIn Optional<UserAccount> userAccount) {
 		long id = Long.parseLong(request.getParameter("id"));
 		String name = request.getParameter("name");
@@ -136,6 +172,28 @@ public class GoodsManagementController {
   	goodToBeUpdated.setPicture(GoodEntity.createPicture(picture));
 		goodToBeUpdated.setUser(loggedUser);
 		
+		GoodValidator goodValidator = new GoodValidator();
+    goodValidator.validate(goodToBeUpdated, bindingResult);
+    /*
+     * If there are errors in the parameters of the good to be updated, the user
+     * is redirected to the good-update form again.
+     */
+    if (bindingResult.hasErrors()) {
+      System.out.println("Invalid good: " + 
+                         bindingResult.getAllErrors().toString());
+      redirectAttributes.addFlashAttribute("id", id);
+      return "redirect:update";
+    }
+    
+    /*
+     * If the good's owner is different from the user trying to update the good,
+     * the user is redirected to its offered goods again.
+     */
+    if (goodToBeUpdated.getUser() != loggedUser) {
+      model.addAttribute("wantedAction", "update");
+      return "notYourGood";
+    }
+    
 		loggedUser.addGood(goodToBeUpdated);
     userRepository.save(loggedUser);
 
@@ -166,14 +224,24 @@ public class GoodsManagementController {
     
 		GoodEntity good = goodsRepository.findOne(id);
 
-		// This statement check if the entity to delete actually exists.
-		if (good != null) {
-		  loggedUser.removeGood(good);
-	    userRepository.save(loggedUser);
-		  goodsRepository.delete(id);
-		}
-    // If the entity doesn't exist, an empty entity is returned.
-  	else good = GoodEntity.createEmptyGood();
+		// If the good is null, the user is redirected to its offered goods again.
+    if (good == null) {
+      model.addAttribute("wantedAction", "delete");
+      return "noSuchGood";
+    }
+    
+    /*
+     * If the good's owner is different from the user trying to delete the good,
+     * the user is redirected to its offered goods again.
+     */
+    if (good.getUser() != loggedUser) {
+      model.addAttribute("wantedAction", "delete");
+      return "notYourGood";
+    }
+    
+	  loggedUser.removeGood(good);
+    userRepository.save(loggedUser);
+	  goodsRepository.delete(id);
 
     model.addAttribute("result", good);
 		return "deletedGood";
---
+++ app/src/main/java/validator/GoodValidator.java
@@ -0,0 +1,50 @@
+package validator;
+
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+import app.model.GoodEntity;
+
+/**
+* <h1>GoodValidator</h1>
+* The GoodValidator is used to validate if a good has all the required 
+* attributes under certain conditions.
+*
+* @author Alejandro Sánchez Aristizábal
+* @since  15.12.2015
+*/
+public class GoodValidator implements Validator {
+  
+  /**
+   * Supports.
+   * @param Class<?> The Class that this Validator is being asked if it can 
+   *                 validate
+   * @return boolean The status of the validation possibility
+   */
+  @Override
+  public boolean supports(Class<?> clazz) {
+    return GoodEntity.class.equals(clazz);
+  }
+
+  /**
+   * This method validates if the properties of a good are correct under certain
+   * conditions and also detects error and returns the respective messages.
+   * @param Object The object that is to be validated 
+   * @param Errors The contextual state about the validation process
+   * @return Nothing
+   */
+  @Override
+  public void validate(Object target, Errors errors) {
+    if (target != null) {
+      GoodEntity good = (GoodEntity) target;
+      
+      if (good.getName().trim().isEmpty()) {
+        errors.rejectValue("name", "your_error_code");
+      }
+      if (good.getDescription().trim().isEmpty()) {
+        errors.rejectValue("description", "your_error_code");
+      }
+      if (good.getTag() == null) errors.rejectValue("tag", "your_error_code");
+    }
+  }
+  
+}
