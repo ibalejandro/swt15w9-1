482318eab8f2ed38c35595226853855f5cb50951
Kilian Heret <kilian.heret@mailbox.tu-dresden.de>
2016-01-09 15:06:51 +0100

    Model für die Registrierung angelegt.

---
+++ app/src/main/java/app/model/Registration.java
@@ -0,0 +1,18 @@
+package app.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+
+/**
+ * <h1>Registration</h1>
+ *
+ * @author Kilian Heret
+ * 
+ */
+
+@SuppressWarnings("serial")
+@Entity
+public class Registration implements Serializable {
+
+}
