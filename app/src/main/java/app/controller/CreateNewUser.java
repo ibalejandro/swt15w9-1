b5869575d691a123767d15f196f19cd763d85045
Kilian Heret <kilian.heret@mailbox.tu-dresden.de>
2016-01-09 15:31:31 +0100

    Mailsenden in die Helpfunctions verschoben

--- app/src/main/java/app/controller/CreateNewUser.java
+++ app/src/main/java/app/controller/CreateNewUser.java
@@ -241,84 +241,6 @@ public class CreateNewUser {
 
 	}
 
-	private void Mailsenden(String SendTo, String Subject, String Text) throws MessagingException, IOException {
-
-		String result = "";
-		InputStream inputStream = null;
-
-		String mailhost = "";
-		String mailport = "";
-		String mailusername = "";
-		String mailpassword = "";
-
-		try {
-			Properties prop = new Properties();
-			String propFileName = "application.properties";
-
-			inputStream = getClass().getClassLoader().getResourceAsStream(propFileName);
-
-			if (inputStream != null) {
-				prop.load(inputStream);
-			} else {
-				throw new FileNotFoundException("property file '" + propFileName + "' not found in the classpath");
-			}
-
-			Date time = new Date(System.currentTimeMillis());
-
-			// get the property value and print it out
-			mailhost = prop.getProperty("spring.mail.host");
-			mailport = prop.getProperty("spring.mail.port");
-			mailusername = prop.getProperty("spring.mail.username");
-			mailpassword = prop.getProperty("spring.mail.password");
-
-		} catch (Exception e) {
-			System.out.println("Exception: " + e);
-		} finally {
-			inputStream.close();
-		}
-
-		// String mailhost = "***"; //Aus application.properties auslesen.
-		// String mailusername = "***"; //Aus application.properties auslesen.
-		// String mailpassword = "***"; //Aus application.properties auslesen.
-		// String mailport= "***"; //Aus application.properties auslesen.
-		String recipient = SendTo;
-
-		Properties props = new Properties();
-
-		props.put("mail.smtp.host", mailhost);
-		props.put("mail.from", mailusername);
-		props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.port", mailport);
-		props.setProperty("mail.debug", "false");
-
-		Session session = Session.getInstance(props, null);
-		MimeMessage msg = new MimeMessage(session);
-
-		msg.setRecipients(Message.RecipientType.TO, recipient);
-		msg.setSubject(Subject);
-		msg.setSentDate(new Date());
-		msg.setContent(Text, "text/html; charset=utf-8");
-
-		Transport transport = session.getTransport("smtp");
-
-		transport.connect(mailusername, mailpassword);
-		transport.sendMessage(msg, msg.getAllRecipients());
-		transport.close();
-
-		return;
-
-	}
-
-	private String AktivierungskeyErzeugen(String username, String mail, Integer Zufallszahl1, Integer Zufallszahl2) {
-		float fl = Zufallszahl1 / Zufallszahl2;
-		String starttext = HelpFunctions.sha256("s" + HelpFunctions.sha256(
-				HelpFunctions.sha256("Aktivierungskey" + username + "123" + mail + "XYZ" + Float.toString(fl) + "fff")
-						+ Zufallszahl1.toString())
-				+ Zufallszahl2.toString());
-		return HelpFunctions.sha256(
-				HelpFunctions.sha256(HelpFunctions.sha256(HelpFunctions.sha256(HelpFunctions.sha256(starttext)))));
-	}
-
 	@RequestMapping(value = "/submit_captcha", method = RequestMethod.POST)
 	public String recieve_reCAPTCHA(@RequestParam("g-recaptcha-response") String CaptchaResponse) {
 
@@ -416,7 +338,7 @@ public class CreateNewUser {
 					if (!mailadresse.equals("test@test.test")) {
 						// Mail senden:
 						try {
-							Mailsenden(mailadresse, "Activation of your RefugeesApp-Account ("
+							HelpFunctions.Mailsenden(mailadresse, "Activation of your RefugeesApp-Account ("
 									+ user_xyz.getUserAccount().getUsername() + ")", mailtext);
 							System.out.println("Mail versandt");
 						} catch (MessagingException | IOException e) {
@@ -574,91 +496,6 @@ public class CreateNewUser {
 		}
 	}
 
-	private static boolean containsString(String s, String subString) {
-		return s.indexOf(subString) > -1 ? true : false;
-	}
-
-	private boolean emailValidator(String email) {
-		boolean isValid = false;
-
-		if (containsString(email, "@") == false) {
-			return false;
-		}
-
-		if (containsString(email, ".") == false) {
-			return false;
-		}
-
-		/*
-		 * if (email.equals("test@test.test")) { return false; }
-		 */
-
-		try {
-			//
-			// Create InternetAddress object and validated the supplied
-			// address which is this case is an email address.
-			InternetAddress internetAddress = new InternetAddress(email);
-			internetAddress.validate();
-			isValid = true;
-		} catch (AddressException e) {
-			System.out.println("You are in catch block -- Exception Occurred for: " + email);
-		}
-		return isValid;
-	}
-
-	private int checkPasswordStrength(String password) {
-		if (password.equals("")) {
-			return 0;
-		}
-
-		boolean hasLower = false;
-		boolean hasUpper = false;
-		boolean hasDigits = false;
-		boolean hasSymbols = false;
-
-		float strengthPercentage = 0;
-		String[] partialRegexChecks = { ".*[a-z]+.*", // lower
-				".*[A-Z]+.*", // upper
-				".*[\\d]+.*", // digits
-				".*[@#§$%&/()=?{}#+-~.,;:<>|\\!]+.*" // symbols
-		};
-
-		int i = 0;
-		while (i < (password.length())) {
-
-			if (password.substring(i, i + 1).matches(partialRegexChecks[0])) {
-				hasLower = true;
-				strengthPercentage += 2;
-			}
-			if (password.substring(i, i + 1).matches(partialRegexChecks[1])) {
-				hasUpper = true;
-				strengthPercentage += 3;
-			}
-			if (password.substring(i, i + 1).matches(partialRegexChecks[2])) {
-				hasDigits = true;
-				strengthPercentage += 5;
-			}
-			if ((!password.substring(i, i + 1).matches(partialRegexChecks[0]))
-					&& (!password.substring(i, i + 1).matches(partialRegexChecks[1]))
-					&& (!password.substring(i, i + 1).matches(partialRegexChecks[2]))) {
-				if (password.substring(i, i + 1).matches(partialRegexChecks[3])) {
-					hasSymbols = true;
-					strengthPercentage += 8;
-				}
-			}
-			i = i + 1;
-		}
-
-		// 12 Zeichen empfohlen
-		int pwlengthsec = (int) Math.floor(Math.floor(Math.log((password.length() - 6) * 0.00002)) + 11);
-		strengthPercentage = strengthPercentage * (1 + (pwlengthsec / 10)) * 3.0f;
-
-		if (!((hasLower && hasUpper) && (hasDigits && hasSymbols) && (password.length() >= 8))) {
-			strengthPercentage = 0;
-		}
-		return Math.round(strengthPercentage);
-	}
-
 	// ******************************************************************************************//*************************************************//
 	// Registrierung:
 
@@ -703,7 +540,7 @@ public class CreateNewUser {
 			return "errorpage_wrongpw";
 		} else {
 			int pwstrength = 0;
-			pwstrength = checkPasswordStrength(Password);
+			pwstrength = HelpFunctions.checkPasswordStrength(Password);
 
 			System.out.println("PasswordStrength: " + pwstrength);
 			if (pwstrength == 0) {
@@ -712,7 +549,7 @@ public class CreateNewUser {
 			}
 		}
 
-		if (emailValidator(Mail) == false) {
+		if (HelpFunctions.emailValidator(Mail) == false) {
 			System.out.println("E: " + Mail + " ist eine ungültige Mailadresse.");
 			return "error";
 		}
@@ -998,7 +835,7 @@ public class CreateNewUser {
 		z1 = (int) (Math.random() * 1000000000) + 123456;
 		z2 = (int) (Math.random() * 1000000000) + 117980;
 
-		String activationkey = AktivierungskeyErzeugen(user_xyz.getUserAccount().getUsername(),
+		String activationkey = HelpFunctions.AktivierungskeyErzeugen(user_xyz.getUserAccount().getUsername(),
 				user_xyz.getUserAccount().getEmail(), z1, z2);
 		user_xyz.setActivationkey(activationkey);
 
@@ -1063,7 +900,7 @@ public class CreateNewUser {
 				if (!mailadresse.equals("test@test.test")) {
 					// Mail senden:
 					try {
-						Mailsenden(mailadresse, "Activation of your RefugeesApp-Account ("
+						HelpFunctions.Mailsenden(mailadresse, "Activation of your RefugeesApp-Account ("
 								+ user_xyz.getUserAccount().getUsername() + ")", mailtext);
 						System.out.println("Mail versandt");
 					} catch (MessagingException | IOException e) {
@@ -1119,7 +956,7 @@ public class CreateNewUser {
 			return "errorpage0_wrongpw";
 		} else {
 			int pwstrength = 0;
-			pwstrength = checkPasswordStrength(Password);
+			pwstrength = HelpFunctions.checkPasswordStrength(Password);
 
 			System.out.println("PasswordStrength: " + pwstrength);
 			if (pwstrength == 0) {
@@ -1128,7 +965,7 @@ public class CreateNewUser {
 			}
 		}
 
-		if (emailValidator(Mail) == false) {
+		if (HelpFunctions.emailValidator(Mail) == false) {
 			System.out.println(Mail + " ist eine ungültige Mailadresse.");
 			return "error";
 		}
@@ -1404,7 +1241,7 @@ public class CreateNewUser {
 			z1 = (int) (Math.random() * 1000000000) + 123456;
 			z2 = (int) (Math.random() * 1000000000) + 117980;
 
-			String activationkey = AktivierungskeyErzeugen(user_xyz.getUserAccount().getUsername(),
+			String activationkey = HelpFunctions.AktivierungskeyErzeugen(user_xyz.getUserAccount().getUsername(),
 					user_xyz.getUserAccount().getEmail(), z1, z2);
 			user_xyz.setActivationkey(activationkey);
 
