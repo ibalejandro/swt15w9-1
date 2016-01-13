eb0f4d15425d9083a566e6acde3a8545a8fb453d
Mario Henze <mario.henze.94@gmail.com>
2016-01-13 23:41:34 +0100

    Removed regular expression check of textblocks

--- app/src/test/java/app/test/Dialog/FormatStringTagFilterTest.java
+++ app/src/test/java/app/test/Dialog/FormatStringTagFilterTest.java
@@ -3,14 +3,12 @@
  */
 package app.test.Dialog;
 
-import static org.junit.Assert.fail;
-
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
+import app.textblocks.FormatTag;
 import app.util.FormatStringTagFilter;
 
 /**
@@ -18,28 +16,6 @@ import app.util.FormatStringTagFilter;
  *
  */
 public class FormatStringTagFilterTest {
-	private static Iterable<String> validExamples;
-	private static Iterable<String> invalidExamples;
-
-	/**
-	 * @throws java.lang.Exception
-	 */
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-		// Add valid examples here that should pass the test
-		List<String> vl = new ArrayList<>();
-		vl.add("The ${animal} jumped over the ${target}. Now that's ${adj}.");
-		vl.add("${animal}");
-		vl.add("animal");
-		validExamples = vl;
-
-		// Add invalid examples here that should not pass the test
-		List<String> il = new ArrayList<>();
-		il.add("The ${anim&$$al} jumpe$d over the ${target}. Now that's ${adj}.");
-		il.add("$");
-		invalidExamples = il;
-	}
-
 	/**
 	 * Test method for
 	 * {@link app.util.FormatStringTagFilter#FormatStringTagFilter(java.lang.String)}
@@ -55,31 +31,13 @@ public class FormatStringTagFilterTest {
 	 */
 	@Test
 	public void testGetTags() {
-		/*FormatStringTagFilter f = new FormatStringTagFilter("The ${animal} jumped over the ${target}. Now that's ${adj}.");
+		FormatStringTagFilter f = new FormatStringTagFilter("The ${animal} jumped over the ${target}. Now that's ${adj}.");
+		String[] ct = {"animal", "target", "adj"};
+		List<String> checkTags = Arrays.asList(ct);
 		List<FormatTag> l = f.getTags();
 		
 		for (FormatTag formatTag : l) {
-			
-		}*/
-		fail("Not implemented yet.");
-	}
-
-	/**
-	 * Test method for
-	 * {@link app.util.FormatStringTagFilter#isValidFormatString(java.lang.String)}
-	 * .
-	 */
-	@Test
-	public void testIsValidFormatString() {
-		for (String st : validExamples) {
-			if (!(FormatStringTagFilter.isValidFormatString(st)))
-				fail("Not every valid example recognized: " + st);
-		}
-		
-		for (String st : invalidExamples) {
-			if ((FormatStringTagFilter.isValidFormatString(st)))
-				fail("An invalid example was recognized: " + st);
+			checkTags.contains(formatTag);
 		}
 	}
-
 }
